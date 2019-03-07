d7c7ebcf1075d8bfe9d9f035a27ebb855c4724b8
Mila-Zagrevskaya <mila.zagrevskaya@gmail.com>
2019-03-07 14:22:16 +0200

    исправлена ошибка

--- js/index4.3.js
+++ js/index4.3.js
@@ -1,14 +1,15 @@
 function Constr ( age, hobby ){
   this.age = age || 24
   this.hobby = hobby || "painting"
-  this.addProperty = function( propNname, value){
-    this[propNname] = value
-  }
 }
 var human = new Constr (25)
 var human2 = new Constr (18, "football")
 console.log (human)
 console.log (human2)
+
+Constr.prototype.addProperty = function( propNname, value){
+    this[propNname] = value
+  }
 
 human.addProperty ("name", "Masha")
 console.log (human)
